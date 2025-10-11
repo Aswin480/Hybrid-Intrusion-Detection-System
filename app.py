fac040bbb39cbf0dff054accacf1886725b56386
Naveen Jayaraj <naveenpainthouse@gmail.com>
2025-10-12 03:31:02 +0530

    added sample data

--- app.py
+++ app.py
@@ -169,14 +169,33 @@ tab1, tab2, tab3 = st.tabs(["🔬 Live Analysis", "📈 Reporting & Analytics", 
 # TAB 1: Live Analysis
 # ==========================================================
 with tab1:
+    # --- Samples from the CSV will be loaded here ---
+    df = pd.read_csv('sampled_by_label.csv')
+    df.columns = df.columns.str.strip()
+
+
+    benign_samples = [row.to_json() for index, row in df[df['Label'] == 'BENIGN'].iterrows()]
+    attack_samples = [row.to_json() for index, row in df[df['Label'] != 'BENIGN'].iterrows()]
+
+    stateless_samples = [
+    json.dumps({'Destination Port': 80, 'Flow Duration': 500000, 'Total Fwd Packets': 60, 'Total Backward Packets': 1, 'Total Length of Fwd Packets': 20, 'Total Length of Bwd Packets': 20, 'Fwd Packet Length Max': 20, 'Fwd Packet Length Min': 20, 'Fwd Packet Length Mean': 20.0, 'Fwd Packet Length Std': 0.0, 'Bwd Packet Length Max': 20, 'Bwd Packet Length Min': 20, 'Bwd Packet Length Mean': 20.0, 'Bwd Packet Length Std': 0.0, 'Flow Bytes/s': 400.0, 'Flow Packets/s': 20.0, 'Flow IAT Mean': 100.0, 'Flow IAT Std': 0.0, 'Flow IAT Max': 100, 'Flow IAT Min': 100, 'Fwd IAT Total': 100, 'Fwd IAT Mean': 100.0, 'Fwd IAT Std': 0.0, 'Fwd IAT Max': 100, 'Fwd IAT Min': 100, 'Bwd IAT Total': 0, 'Bwd IAT Mean': 0.0, 'Bwd IAT Std': 0.0, 'Bwd IAT Max': 0, 'Bwd IAT Min': 0, 'Fwd PSH Flags': 0, 'Bwd PSH Flags': 0, 'Fwd URG Flags': 0, 'Bwd URG Flags': 0, 'Fwd Header Length': 20, 'Bwd Header Length': 20, 'Fwd Packets/s': 10.0, 'Bwd Packets/s': 10.0, 'Min Packet Length': 20, 'Max Packet Length': 20, 'Packet Length Mean': 20.0, 'Packet Length Std': 0.0, 'Packet Length Variance': 0.0, 'FIN Flag Count': 0, 'SYN Flag Count': 1, 'RST Flag Count': 0, 'PSH Flag Count': 0, 'ACK Flag Count': 0, 'URG Flag Count': 0, 'CWE Flag Count': 0, 'ECE Flag Count': 0, 'Down/Up Ratio': 1, 'Average Packet Size': 30.0, 'Avg Fwd Segment Size': 20.0, 'Avg Bwd Segment Size': 20.0, 'Fwd Header Length.1': 20, 'Fwd Avg Bytes/Bulk': 0, 'Fwd Avg Packets/Bulk': 0, 'Fwd Avg Bulk Rate': 0, 'Bwd Avg Bytes/Bulk': 0, 'Bwd Avg Packets/Bulk': 0, 'Bwd Avg Bulk Rate': 0, 'Subflow Fwd Packets': 1, 'Subflow Fwd Bytes': 20, 'Subflow Bwd Packets': 1, 'Subflow Bwd Bytes': 20, 'Init_Win_bytes_forward': 8192, 'Init_Win_bytes_backward': 8192, 'act_data_pkt_fwd': 1, 'min_seg_size_forward': 20, 'Active Mean': 0.0, 'Active Std': 0.0, 'Active Max': 0, 'Active Min': 0, 'Idle Mean': 0.0, 'Idle Std': 0.0, 'Idle Max': 0, 'Idle Min': 0}, indent=4),
+    json.dumps({'Destination Port': 80, 'Flow Duration': 600000, 'Total Fwd Packets': 75, 'Total Backward Packets': 2, 'Total Length of Fwd Packets': 40, 'Total Length of Bwd Packets': 40, 'Fwd Packet Length Max': 20, 'Fwd Packet Length Min': 20, 'Fwd Packet Length Mean': 20.0, 'Fwd Packet Length Std': 0.0, 'Bwd Packet Length Max': 20, 'Bwd Packet Length Min': 20, 'Bwd Packet Length Mean': 20.0, 'Bwd Packet Length Std': 0.0, 'Flow Bytes/s': 400.0, 'Flow Packets/s': 20.0, 'Flow IAT Mean': 50.0, 'Flow IAT Std': 28.86, 'Flow IAT Max': 100, 'Flow IAT Min': 0, 'Fwd IAT Total': 200, 'Fwd IAT Mean': 100.0, 'Fwd IAT Std': 0.0, 'Fwd IAT Max': 100, 'Fwd IAT Min': 100, 'Bwd IAT Total': 100, 'Bwd IAT Mean': 50.0, 'Bwd IAT Std': 0.0, 'Bwd IAT Max': 50, 'Bwd IAT Min': 50, 'Fwd PSH Flags': 0, 'Bwd PSH Flags': 0, 'Fwd URG Flags': 0, 'Bwd URG Flags': 0, 'Fwd Header Length': 40, 'Bwd Header Length': 40, 'Fwd Packets/s': 10.0, 'Bwd Packets/s': 10.0, 'Min Packet Length': 20, 'Max Packet Length': 20, 'Packet Length Mean': 20.0, 'Packet Length Std': 0.0, 'Packet Length Variance': 0.0, 'FIN Flag Count': 0, 'SYN Flag Count': 1, 'RST Flag Count': 0, 'PSH Flag Count': 1, 'ACK Flag Count': 1, 'URG Flag Count': 0, 'CWE Flag Count': 0, 'ECE Flag Count': 0, 'Down/Up Ratio': 1, 'Average Packet Size': 25.0, 'Avg Fwd Segment Size': 20.0, 'Avg Bwd Segment Size': 20.0, 'Fwd Header Length.1': 40, 'Fwd Avg Bytes/Bulk': 0, 'Fwd Avg Packets/Bulk': 0, 'Fwd Avg Bulk Rate': 0, 'Bwd Avg Bytes/Bulk': 0, 'Bwd Avg Packets/Bulk': 0, 'Bwd Avg Bulk Rate': 0, 'Subflow Fwd Packets': 2, 'Subflow Fwd Bytes': 40, 'Subflow Bwd Packets': 2, 'Subflow Bwd Bytes': 40, 'Init_Win_bytes_forward': 4096, 'Init_Win_bytes_backward': 4096, 'act_data_pkt_fwd': 2, 'min_seg_size_forward': 20, 'Active Mean': 0.0, 'Active Std': 0.0, 'Active Max': 0, 'Active Min': 0, 'Idle Mean': 0.0, 'Idle Std': 0.0, 'Idle Max': 0, 'Idle Min': 0}, indent=4)
+]
+
+    stateful_samples = [
+        json.dumps({"Source IP": "10.0.0.5", 'Destination Port': 22, 'Flow Duration': 100, 'Total Fwd Packets': 1, 'Total Backward Packets': 1, 'num_failed_logins': 1, 'logged_in': 0, 'Total Length of Fwd Packets': 20, 'Total Length of Bwd Packets': 20, 'Fwd Packet Length Max': 20, 'Fwd Packet Length Min': 20, 'Fwd Packet Length Mean': 20.0, 'Fwd Packet Length Std': 0.0, 'Bwd Packet Length Max': 20, 'Bwd Packet Length Min': 20, 'Bwd Packet Length Mean': 20.0, 'Bwd Packet Length Std': 0.0, 'Flow Bytes/s': 400.0, 'Flow Packets/s': 20.0, 'Flow IAT Mean': 100.0, 'Flow IAT Std': 0.0, 'Flow IAT Max': 100, 'Flow IAT Min': 100, 'Fwd IAT Total': 100, 'Fwd IAT Mean': 100.0, 'Fwd IAT Std': 0.0, 'Fwd IAT Max': 100, 'Fwd IAT Min': 100, 'Bwd IAT Total': 0, 'Bwd IAT Mean': 0.0, 'Bwd IAT Std': 0.0, 'Bwd IAT Max': 0, 'Bwd IAT Min': 0, 'Fwd PSH Flags': 0, 'Bwd PSH Flags': 0, 'Fwd URG Flags': 0, 'Bwd URG Flags': 0, 'Fwd Header Length': 20, 'Bwd Header Length': 20, 'Fwd Packets/s': 10.0, 'Bwd Packets/s': 10.0, 'Min Packet Length': 20, 'Max Packet Length': 20, 'Packet Length Mean': 20.0, 'Packet Length Std': 0.0, 'Packet Length Variance': 0.0, 'FIN Flag Count': 0, 'SYN Flag Count': 1, 'RST Flag Count': 0, 'PSH Flag Count': 0, 'ACK Flag Count': 0, 'URG Flag Count': 0, 'CWE Flag Count': 0, 'ECE Flag Count': 0, 'Down/Up Ratio': 1, 'Average Packet Size': 30.0, 'Avg Fwd Segment Size': 20.0, 'Avg Bwd Segment Size': 20.0, 'Fwd Header Length.1': 20, 'Fwd Avg Bytes/Bulk': 0, 'Fwd Avg Packets/Bulk': 0, 'Fwd Avg Bulk Rate': 0, 'Bwd Avg Bytes/Bulk': 0, 'Bwd Avg Packets/Bulk': 0, 'Bwd Avg Bulk Rate': 0, 'Subflow Fwd Packets': 1, 'Subflow Fwd Bytes': 20, 'Subflow Bwd Packets': 1, 'Subflow Bwd Bytes': 20, 'Init_Win_bytes_forward': 8192, 'Init_Win_bytes_backward': 8192, 'act_data_pkt_fwd': 1, 'min_seg_size_forward': 20, 'Active Mean': 0.0, 'Active Std': 0.0, 'Active Max': 0, 'Active Min': 0, 'Idle Mean': 0.0, 'Idle Std': 0.0, 'Idle Max': 0, 'Idle Min': 0}, indent=4),
+        json.dumps({"Source IP": "10.0.0.5", 'Destination Port': 22, 'Flow Duration': 100, 'Total Fwd Packets': 1, 'Total Backward Packets': 1, 'num_failed_logins': 1, 'logged_in': 0, 'Total Length of Fwd Packets': 20, 'Total Length of Bwd Packets': 20, 'Fwd Packet Length Max': 20, 'Fwd Packet Length Min': 20, 'Fwd Packet Length Mean': 20.0, 'Fwd Packet Length Std': 0.0, 'Bwd Packet Length Max': 20, 'Bwd Packet Length Min': 20, 'Bwd Packet Length Mean': 20.0, 'Bwd Packet Length Std': 0.0, 'Flow Bytes/s': 400.0, 'Flow Packets/s': 20.0, 'Flow IAT Mean': 100.0, 'Flow IAT Std': 0.0, 'Flow IAT Max': 100, 'Flow IAT Min': 100, 'Fwd IAT Total': 100, 'Fwd IAT Mean': 100.0, 'Fwd IAT Std': 0.0, 'Fwd IAT Max': 100, 'Fwd IAT Min': 100, 'Bwd IAT Total': 0, 'Bwd IAT Mean': 0.0, 'Bwd IAT Std': 0.0, 'Bwd IAT Max': 0, 'Bwd IAT Min': 0, 'Fwd PSH Flags': 0, 'Bwd PSH Flags': 0, 'Fwd URG Flags': 0, 'Bwd URG Flags': 0, 'Fwd Header Length': 20, 'Bwd Header Length': 20, 'Fwd Packets/s': 10.0, 'Bwd Packets/s': 10.0, 'Min Packet Length': 20, 'Max Packet Length': 20, 'Packet Length Mean': 20.0, 'Packet Length Std': 0.0, 'Packet Length Variance': 0.0, 'FIN Flag Count': 0, 'SYN Flag Count': 0, 'RST Flag Count': 0, 'PSH Flag Count': 0, 'ACK Flag Count': 1, 'URG Flag Count': 0, 'CWE Flag Count': 0, 'ECE Flag Count': 0, 'Down/Up Ratio': 1, 'Average Packet Size': 30.0, 'Avg Fwd Segment Size': 20.0, 'Avg Bwd Segment Size': 20.0, 'Fwd Header Length.1': 20, 'Fwd Avg Bytes/Bulk': 0, 'Fwd Avg Packets/Bulk': 0, 'Fwd Avg Bulk Rate': 0, 'Bwd Avg Bytes/Bulk': 0, 'Bwd Avg Packets/Bulk': 0, 'Bwd Avg Bulk Rate': 0, 'Subflow Fwd Packets': 1, 'Subflow Fwd Bytes': 20, 'Subflow Bwd Packets': 1, 'Subflow Bwd Bytes': 20, 'Init_Win_bytes_forward': 8192, 'Init_Win_bytes_backward': 8192, 'act_data_pkt_fwd': 1, 'min_seg_size_forward': 20, 'Active Mean': 0.0, 'Active Std': 0.0, 'Active Max': 0, 'Active Min': 0, 'Idle Mean': 0.0, 'Idle Std': 0.0, 'Idle Max': 0, 'Idle Min': 0}, indent=4),
+    ]
+
     sample_packets = {
-        "dfa": json.dumps({"Destination Port": 80, "Flow Duration": 298564410, "FIN Flag Count": 0, "SYN Flag Count": 1, "PSH Flag Count": 1}, indent=4),
-        "failed_login": json.dumps({"Source IP": "10.0.0.5", "Destination Port": 22, "num_failed_logins": 1, "logged_in": 0}, indent=4),
-        "ml_attack": json.dumps({'Destination Port': 8080, 'Flow Duration': 10, 'Flow Bytes/s': 2048000.0, 'Flow Packets/s': 200000.0}, indent=4),
-        "benign": json.dumps({'Destination Port': 443, 'Flow Duration': 1200000, 'logged_in': 1, 'FIN Flag Count': 1, "SYN Flag Count": 0}, indent=4),
+        "Stateless (DFA)": stateless_samples,
+        "Stateful (Login)": stateful_samples,
+        "ML Attack": attack_samples,
+        "Benign": benign_samples,
     }
 
-    all_samples = list(sample_packets.values())
+    all_samples = [packet for packets in sample_packets.values() for packet in packets]
+
 
     # --- Safe Widget Mirror ---
     st.session_state.is_simulating = st.session_state.get("is_simulating_ui", False)
@@ -203,11 +222,10 @@ with tab1:
             )
 
             st.markdown("<h6>Load a sample packet:</h6>", unsafe_allow_html=True)
-            bcols = st.columns(4)
-            bcols[0].button("Stateless (DFA)", on_click=lambda: st.session_state.update(packet_input_ui=sample_packets["dfa"]), disabled=st.session_state.is_simulating)
-            bcols[1].button("Stateful (Login)", on_click=lambda: st.session_state.update(packet_input_ui=sample_packets["failed_login"]), disabled=st.session_state.is_simulating)
-            bcols[2].button("ML Attack", on_click=lambda: st.session_state.update(packet_input_ui=sample_packets["ml_attack"]), disabled=st.session_state.is_simulating)
-            bcols[3].button("Benign", on_click=lambda: st.session_state.update(packet_input_ui=sample_packets["benign"]), disabled=st.session_state.is_simulating)
+            bcols = st.columns(len(sample_packets))
+            for i, (label, packets) in enumerate(sample_packets.items()):
+                bcols[i].button(label, on_click=lambda p=packets: st.session_state.update(packet_input_ui=random.choice(p)), disabled=st.session_state.is_simulating)
+
 
             if st.button("Analyze Packet", type="primary", use_container_width=True, disabled=st.session_state.is_simulating):
                 if not packet_data_input:
